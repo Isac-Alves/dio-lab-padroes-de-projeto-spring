2723c575f4c19faf74ebb1fe321a7b56c80070f4
Isac-Alves <isacalves2011@gmail.com>
2025-07-11 09:57:25 -0300

    Criar projeto

---
+++ src/main/java/lab/padroes/de/projeto/spring/LabPadroesDeProjetoSpringApplication.java
@@ -0,0 +1,13 @@
+package lab.padroes.de.projeto.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabPadroesDeProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabPadroesDeProjetoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lab/padroes/de/projeto/spring/LabPadroesDeProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package lab.padroes.de.projeto.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabPadroesDeProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
